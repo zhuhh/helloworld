e9fb3ca6cdd15641f6433d2380dee814492659ca
zhuhh <kerry200408@gmail.com>
2018-03-01 18:10:13 +0800

    a test hello world

---
+++ src/main/java/cn/hh/HelloWorldApp.java
@@ -0,0 +1,17 @@
+/*
+ * CopyRight © 2018 YASN All Rights Reserved.
+ */
+package cn.hh;
+
+/**
+ * @author ZhuHonghao
+ * @version 1.0 2018/3/1
+ * @since 1.8
+ */
+public class HelloWorldApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
